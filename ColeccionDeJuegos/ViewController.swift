51993ce582d4db2f879c12099c7c399340975052
yonnyre <yonny.rivera@tecsup.edu.pe>
2018-05-06 13:52:58 -0500

    first commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by yonny on 6/05/18.
+//  Copyright © 2018 tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
